4cb6762f4793413ac997e2a17fd19e43cfac57b2
uditdesai <udit.desai3@gmail.com>
2022-01-20 22:17:19 -0500

    make aws values env configs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  env: {
+    aws_project_region: "ca-central-1",
+    aws_cognito_identity_pool_id:
+      "ca-central-1:f0920e2b-dde9-450e-8549-ac575a660775",
+    aws_cognito_region: "ca-central-1",
+    aws_user_pools_id: "ca-central-1_AqhhluHxU",
+    aws_user_pools_web_client_id: "1ldmq8aho7s8qat2a4921dg3q3",
+  },
+};
